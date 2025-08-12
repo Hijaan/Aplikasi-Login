afab754d411bb9d43651db7b73fa69394cb6d9db
Dialdz <123873249+Hijaan@users.noreply.github.com>
2025-08-12 21:33:13 +0800

    Menambahkan komentar pada code dan menggunakkan format yang support unutk JSDocs

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -4,18 +4,33 @@
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
-
+/** @module Login-Script */
+/**
+ * @constant {HTMLElement}
+ */
 const loginFormElement = document.querySelector('#loginForm');
+
+/** @constant {HTMLElement} */
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/**@constant {HTMLElement} */
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/** @constant {string} */
 const expectedEmail = 'admin@dicoding.com';
+
+/**@constant {string} */
 const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /* membuat variabel untuk menyimpan nilai dari inputan email pengguna
+  /**@constant {string} */
   const email = inputEmailElement.value;
+
+  /* Membuat varaibel untuk menyimpan nilai dari inputan password pengguna
+  /**@constant {string} */
   const password = inputPasswordElement.value;
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
